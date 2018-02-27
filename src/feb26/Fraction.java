d83dcc909904644cd40f9addf614121a0a57dcb4
topdawgtj <tjsingh51@yahoo.com>
2018-02-27 09:05:58 -0600

    created instance variables, a constructor and compareTo.

--- src/feb26/Fraction.java
+++ src/feb26/Fraction.java
@@ -1,6 +1,28 @@
 package feb26;
 
-public class Fraction
+public class Fraction implements Comparable<Fraction>
 {
-    
+    private int num;
+    private int denom;
+
+
+    public Fraction(int num, int denom)
+    {
+        this.num = num;
+        this.denom = denom;
+        simplify();
+    }
+
+    public int compareTo(Fraction o)
+    {
+        int difference = this.num*o.denom - o.num*this.denom;
+        if (difference > 0) return 1;
+        else if (difference < 0)return - 1;
+        return 0;
+    }
+    private void simplify()
+    {
+
+    }
+
 }
